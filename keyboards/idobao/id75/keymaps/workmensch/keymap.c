7a78d9a7cf4ce0a02a5150f068ecc3c23c080ae3
Jonas A. Hultén <jonas@sldr.se>
2020-04-23 16:55:37 +0200

    Made better use of the auxiliary space bars

--- keyboards/idobao/id75/keymaps/workmensch/keymap.c
+++ keyboards/idobao/id75/keymaps/workmensch/keymap.c
@@ -100,16 +100,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
  * | LSHIFT | Z      | X      | C      | V      | B      | NOOP   | NOOP   | NOOP   | N      | M      | ,      | .      | /      | RSHIFT |
  * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+--------+-----------------+--------+--------|
- * | ESC    | LGUI   | FN     | LALT   | OSL    | SPACE  | SPACE  | CAPS   | SPACE  | SPACE  | OSL    | LEFT   | UP     | DOWN   | RIGHT  |
+ * | ESC    | LGUI   | FN     | LALT   | OSL    | SPACE  | ALGUI  | CAPS   | MEH    | SPACE  | OSL    | LEFT   | UP     | DOWN   | RIGHT  |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
 
   [_WM] = LAYOUT_ortho_5x15( /* Workmensch */
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,       KC_5,    KC_INS,  KC_PSCR, KC_PAUS, KC_6,    KC_7,       KC_8,    KC_9,    KC_0,    KC_BSPC, \
-    KC_TAB,  KC_Q,    KC_D,    KC_R,    KC_W,       KC_B,    KC_MPRV, KC_MPLY, KC_MNXT, KC_J,    KC_F,       KC_U,    KC_P,    KC_SCLN, KC_BSLS, \
-    KC_LCTL, KC_A,    KC_S,    KC_H,    KC_T,       KC_G,    KC_VOLD, KC_MUTE, KC_VOLU, KC_Y,    KC_N,       KC_E,    KC_O,    KC_I,    KC_ENT,  \
-    KC_LSFT, KC_Z,    KC_X,    KC_M,    KC_C,       KC_V,    XXXXXXX, XXXXXXX, XXXXXXX, KC_K,    KC_L,       KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-    KC_ESC,  KC_LGUI, MO(_FN), KC_LALT, OSL(_OSM),  KC_SPC,  KC_SPC,  KC_CAPS, KC_SPC,  KC_SPC,  OSL(_OSM),  KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT  \
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,      KC_5,   KC_INS,     KC_PSCR, KC_PAUS,      KC_6,   KC_7,      KC_8,    KC_9,   KC_0,    KC_BSPC, \
+    KC_TAB,  KC_Q,    KC_D,    KC_R,    KC_W,      KC_B,   KC_MPRV,    KC_MPLY, KC_MNXT,      KC_J,   KC_F,      KC_U,    KC_P,   KC_SCLN, KC_BSLS, \
+    KC_LCTL, KC_A,    KC_S,    KC_H,    KC_T,      KC_G,   KC_VOLD,    KC_MUTE, KC_VOLU,      KC_Y,   KC_N,      KC_E,    KC_O,   KC_I,    KC_ENT,  \
+    KC_LSFT, KC_Z,    KC_X,    KC_M,    KC_C,      KC_V,   XXXXXXX,    XXXXXXX, XXXXXXX,      KC_K,   KC_L,      KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, \
+    KC_ESC,  KC_LGUI, MO(_FN), KC_LALT, OSL(_OSM), KC_SPC, A(KC_LGUI), KC_CAPS, LCA(KC_LSFT), KC_SPC, OSL(_OSM), KC_LEFT, KC_UP,  KC_DOWN, KC_RGHT  \
  ),
 
 /* FUNCTION
