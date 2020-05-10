99c20d3effb3ec4353bc60ed895de4b5db33786c
Jonas A. Hultén <jonas@sldr.se>
2020-05-10 14:25:49 +0200

    Reworked unicode to use unicodemap instead
    Allows for use of Shift, like a normal person would

--- keyboards/idobo/keymaps/workmensch/rules.mk
+++ keyboards/idobo/keymaps/workmensch/rules.mk
@@ -13,6 +13,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-UNICODE_ENABLE = yes
+UNICODEMAP_ENABLE = yes
 MOUSEKEY_ENABLE = no
 NKRO_ENABLE = yes
