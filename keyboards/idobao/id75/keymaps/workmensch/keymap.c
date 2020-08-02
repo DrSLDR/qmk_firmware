61546f8cca2734cfafda0abd64a6f39ca2a0e379
Jonas A. Hultén <jonas@sldr.se>
2020-08-02 16:25:07 +0200

    Added a QWERTY layer, because that's useful

--- keyboards/idobao/id75/keymaps/workmensch/keymap.c
+++ keyboards/idobao/id75/keymaps/workmensch/keymap.c
@@ -17,8 +17,9 @@
 
 // Keyboard Layers
 #define _WM 0
-#define _FN 1
-#define _OSM 2
+#define _QW 1
+#define _FN 2
+#define _OSM 3
 
 // Modifier macros
 #define _MOD_LCTL 0x1
@@ -113,11 +114,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * .--------------------------------------------------------------------------------------------------------------------------------------.
  * | GRAVE  | 1      | 2      | 3      | 4      | 5      | INSERT | PSCR   | BREAK  | 6      | 7      | 8      | 9      | 0      | BACKSP |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
- * | TAB    | Q      | W      | E      | R      | T      | PREV   | PLAY   | NEXT   | Y      | U      | I      | O      | P      | \      |
+ * | TAB    | Q      | D      | R      | W      | B      | PREV   | PLAY   | NEXT   | J      | F      | U      | P      | ;      | \      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
- * | LCTRL  | A      | S      | D      | F      | G      | VOL-   | MUTE   | VOL+   | H      | J      | K      | L      | ;      | ENTER  |
+ * | LCTRL  | A      | S      | H      | T      | G      | VOL-   | MUTE   | VOL+   | Y      | N      | E      | O      | I      | ENTER  |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
- * | LSHIFT | Z      | X      | C      | V      | B      | ALGUI  | NOOP   | LCA    | N      | M      | ,      | .      | /      | RSHIFT |
+ * | LSHIFT | Z      | X      | M      | C      | V      | ALGUI  | NOOP   | LCA    | K      | L      | ,      | .      | /      | RSHIFT |
  * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+--------+-----------------+--------+--------|
  * | ESC/FN | LGUI   | FN     | LALT   | OSL    | SPACE  | CLSFT  | CAPS   | MEH    | SPACE  | OSL    | LEFT   | UP     | DOWN   | RIGHT  |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
@@ -128,6 +129,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,          KC_Q,    KC_D,    KC_R,    KC_W,      KC_B,   KC_MPRV,    KC_MPLY, KC_MNXT,      KC_J,   KC_F,      KC_U,    KC_P,   KC_SCLN, KC_BSLS, \
     KC_LCTL,         KC_A,    KC_S,    KC_H,    KC_T,      KC_G,   KC_VOLD,    KC_MUTE, KC_VOLU,      KC_Y,   KC_N,      KC_E,    KC_O,   KC_I,    KC_ENT,  \
     KC_LSFT,         KC_Z,    KC_X,    KC_M,    KC_C,      KC_V,   A(KC_LGUI), XXXXXXX, C(KC_LALT),   KC_K,   KC_L,      KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, \
+    LT(_FN, KC_ESC), KC_LGUI, MO(_FN), KC_LALT, OSL(_OSM), KC_SPC, C(KC_LSFT), KC_CAPS, LCA(KC_LSFT), KC_SPC, OSL(_OSM), KC_LEFT, KC_UP,  KC_DOWN, KC_RGHT  \
+ ),
+
+ /* QWERTY (Because that's needed sometimes)
+ * .--------------------------------------------------------------------------------------------------------------------------------------.
+ * | GRAVE  | 1      | 2      | 3      | 4      | 5      | INSERT | PSCR   | BREAK  | 6      | 7      | 8      | 9      | 0      | BACKSP |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
+ * | TAB    | Q      | W      | E      | R      | T      | PREV   | PLAY   | NEXT   | Y      | U      | I      | O      | P      | \      |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
+ * | LCTRL  | A      | S      | D      | F      | G      | VOL-   | MUTE   | VOL+   | H      | J      | K      | L      | ;      | ENTER  |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
+ * | LSHIFT | Z      | X      | C      | V      | B      | ALGUI  | NOOP   | LCA    | N      | M      | ,      | .      | /      | RSHIFT |
+ * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+--------+-----------------+--------+--------|
+ * | ESC/FN | LGUI   | FN     | LALT   | OSL    | SPACE  | CLSFT  | CAPS   | MEH    | SPACE  | OSL    | LEFT   | UP     | DOWN   | RIGHT  |
+ * '--------------------------------------------------------------------------------------------------------------------------------------'
+ */
+
+  [_QW] = LAYOUT_ortho_5x15( /* QWERTY */
+    KC_GRV,          KC_1,    KC_2,    KC_3,    KC_4,      KC_5,   KC_INS,     KC_PSCR, KC_PAUS,      KC_6,   KC_7,      KC_8,    KC_9,   KC_0,    KC_BSPC, \
+    KC_TAB,          KC_Q,    KC_W,    KC_E,    KC_R,      KC_T,   KC_MPRV,    KC_MPLY, KC_MNXT,      KC_Y,   KC_U,      KC_I,    KC_O,   KC_P,    KC_BSLS, \
+    KC_LCTL,         KC_A,    KC_S,    KC_D,    KC_F,      KC_G,   KC_VOLD,    KC_MUTE, KC_VOLU,      KC_H,   KC_J,      KC_K,    KC_L,   KC_SCLN, KC_ENT,  \
+    KC_LSFT,         KC_Z,    KC_X,    KC_C,    KC_V,      KC_B,   A(KC_LGUI), XXXXXXX, C(KC_LALT),   KC_N,   KC_M,      KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, \
     LT(_FN, KC_ESC), KC_LGUI, MO(_FN), KC_LALT, OSL(_OSM), KC_SPC, C(KC_LSFT), KC_CAPS, LCA(KC_LSFT), KC_SPC, OSL(_OSM), KC_LEFT, KC_UP,  KC_DOWN, KC_RGHT  \
  ),
 
@@ -272,27 +295,26 @@ void keyboard_post_init_user(){
 // LAYER CONTROL FUNCTION /////////////////////////////////////////////////////
 
 void move_layer(bool up) {
-  // Keep the function for later, but do nothing
-  // switch (active_base_layer) {
-  //   case _WM:
-  //     if (up) {
-  //       active_base_layer = _SE;
-  //       layer_on(active_base_layer);
-  //       rgblight_sethsv(_SE_LED_HSV);
-  //     }
-  //     break;
-  //   case _SE:
-  //     if (up) {
-  //       // There is no higher layer
-  //       return;
-  //     }
-  //     else {
-  //       layer_off(active_base_layer);
-  //       active_base_layer = _WM;
-  //       rgblight_sethsv(_WM_LED_HSV);
-  //     }
-  //     break;
-  // }
+  switch (active_base_layer) {
+    case _WM:
+      if (up) {
+        active_base_layer = _QW;
+        layer_on(active_base_layer);
+        rgblight_sethsv(_QW_LED_HSV);
+      }
+      break;
+    case _QW:
+      if (up) {
+        // There is no higher layer
+        return;
+      }
+      else {
+        layer_off(active_base_layer);
+        active_base_layer = _WM;
+        rgblight_sethsv(_WM_LED_HSV);
+      }
+      break;
+  }
   return;
 }
 
