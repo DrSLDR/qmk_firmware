fed4e1fab3a0bac2b1bc28dfb9f05cf3a6a298cd
Jonas A. Hultén <jonas@sldr.se>
2021-10-19 16:11:56 +0200

    Moved the Meta key

--- keyboards/planck/keymaps/sldr/keymap.c
+++ keyboards/planck/keymaps/sldr/keymap.c
@@ -48,14 +48,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   M  |   C  |   V  |   K  |   L  |   ,  |   .  |   /  | Shift|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Esc  | Ctrl | Meta | Alt  | Lower| Space| Enter| Raise| Left | Up   | Down |Right |
+ * | Esc  | Ctrl | Alt  | Meta | Lower| Space| Enter| Raise| Left | Up   | Down |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_WM] = LAYOUT_planck_grid(
     KC_TAB,  KC_Q,    KC_D,    KC_R,    KC_W,    KC_B,    KC_J,    KC_F,    KC_U,    KC_P,    KC_SCLN, KC_BSLS,
     KC_LCTL, KC_A,    KC_S,    KC_H,    KC_T,    KC_G,    KC_Y,    KC_N,    KC_E,    KC_O,    KC_I,    KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_M,    KC_C,    KC_V,    KC_K,    KC_L,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    KC_ESC,  AU_ON,   KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_ENT,  RAISE,   KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT
+    KC_ESC,  AU_ON,   KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_ENT,  RAISE,   KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT
 ),
 
 /* Lower
