6e8ac08bab0c3c714dae982370ab0d2f16bb86b9
Jonas A. Hultén <jonas@sldr.se>
2022-12-17 14:52:11 +0100

    Stripped arrows

--- keyboards/keebio/iris/keymaps/sldr/config.h
+++ keyboards/keebio/iris/keymaps/sldr/config.h
@@ -36,7 +36,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Combo funstuff */
 
-#define COMBO_COUNT 14
+#define COMBO_COUNT 12
 #define COMBO_TERM 50
 
 /* USB Device descriptor parameter */
