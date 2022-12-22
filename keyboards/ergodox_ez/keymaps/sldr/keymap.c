a823e2973ad9cd36876cdbd23f4e81095b7429d7
Jonas A. Hultén <jonas@sldr.se>
2022-12-23 00:15:07 +0100

    Added a damn escape key somewhere

--- keyboards/ergodox_ez/keymaps/sldr/keymap.c
+++ keyboards/ergodox_ez/keymaps/sldr/keymap.c
@@ -40,11 +40,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [BASE] = LAYOUT_ergodox_pretty(
   // left hand
-  KC_EQL,          KC_1,    KC_2,          KC_3,    KC_4,    KC_5,    KC_LEFT,              KC_RGHT,      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,
-  KC_DEL,          KC_Q,    KC_D,          KC_R,    KC_W,    KC_B,    TG(SYMB),             TG(SYMB),     KC_J,    KC_F,    KC_U,    KC_P,    KC_SCLN,  KC_BSLS,
-  KC_LCTL,         KC_A,    KC_S,          KC_H,    KC_T,    KC_G,                                        KC_Y,    KC_N,    KC_E,    KC_O,    KC_I,     KC_QUOT,
-  KC_LSFT,         KC_Z,    KC_X,          KC_M,    KC_C,    KC_V,    ALL_T(KC_NO),         MEH_T(KC_NO), KC_K,    KC_L,    KC_COMM, KC_DOT,   KC_SLSH, KC_RSFT,
-  LT(SYMB,KC_GRV), KC_QUOT, LALT(KC_LSFT), KC_LEFT, KC_RGHT,                                              KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC, TT(SYMB),
+  KC_EQL,  KC_1,    KC_2,          KC_3,    KC_4,    KC_5,    KC_LEFT,              KC_RGHT,      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,
+  KC_DEL,  KC_Q,    KC_D,          KC_R,    KC_W,    KC_B,    TG(SYMB),             TG(SYMB),     KC_J,    KC_F,    KC_U,    KC_P,    KC_SCLN,  KC_BSLS,
+  KC_LCTL, KC_A,    KC_S,          KC_H,    KC_T,    KC_G,                                        KC_Y,    KC_N,    KC_E,    KC_O,    KC_I,     KC_QUOT,
+  KC_LSFT, KC_Z,    KC_X,          KC_M,    KC_C,    KC_V,    ALL_T(KC_NO),         MEH_T(KC_NO), KC_K,    KC_L,    KC_COMM, KC_DOT,   KC_SLSH, KC_RSFT,
+  KC_ESC,  KC_QUOT, LALT(KC_LSFT), KC_LEFT, KC_RGHT,                                              KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC, TT(SYMB),
                                                            ALT_T(KC_APP), KC_LGUI,                KC_LALT, CTL_T(KC_ESC),
                                                                           KC_HOME,                 KC_PGUP,
                                                          KC_SPC, KC_BSPC, KC_END,                  KC_PGDN, KC_TAB, KC_ENT
