5802ee1f43153210c30932257d5aba59dbb8332e
Jonas A. Hultén <jonas@sldr.se>
2023-01-02 22:37:35 +0100

    Made the leds count binary, as you'd expect

--- keyboards/ergodox_ez/keymaps/sldr/keymap.c
+++ keyboards/ergodox_ez/keymaps/sldr/keymap.c
@@ -285,14 +285,14 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 #endif
             break;
         case 3:
-            ergodox_right_led_3_on();
+            ergodox_right_led_1_on();
+            ergodox_right_led_2_on();
 #ifdef RGBLIGHT_COLOR_LAYER_3
             rgblight_setrgb(RGBLIGHT_COLOR_LAYER_3);
 #endif
             break;
         case 4:
-            ergodox_right_led_1_on();
-            ergodox_right_led_2_on();
+            ergodox_right_led_3_on();
 #ifdef RGBLIGHT_COLOR_LAYER_4
             rgblight_setrgb(RGBLIGHT_COLOR_LAYER_4);
 #endif
