8643e70318780b3ea6ceef1a659f5fc236121b9c
Jonas A. Hultén <jonas@sldr.se>
2023-01-02 18:37:57 +0100

    Revised thumb clusters and modifiers

--- keyboards/ergodox_ez/keymaps/sldr/keymap.c
+++ keyboards/ergodox_ez/keymaps/sldr/keymap.c
@@ -77,32 +77,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |   `    |   1  |   2  |   3  |   4  |   5  | NOOP |           | NOOP |   6  |   7  |   8  |   9  |   0  | Bsp/MED|
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Del    |   Q  |   D  |   R  |   W  |   B  |  SA  |           |  L1  |   J  |   F  |   U  |   P  |   ;  |   \    |
+ * | Del    |   Q  |   D  |   R  |   W  |   B  |  SA  |           |  CA  |   J  |   F  |   U  |   P  |   ;  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | Ctrl   |   A  |   S  |   H  |   T  |   G  |------|           |------|   Y  |   N  |   E  |   O  |   I  |   '    |
  * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   M  |   C  |   V  |      |           |      |   K  |   L  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |Esc/ut| NOOP | NOOP | NOOP | LGui |                                       | UTIL | Left |  Up  | Down | Right |
+ *   |Esc/ut| Alt  | NOOP | NOOP | LGui |                                       | UTIL | Left |  Up  | Down | Right |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        | App  | LGui |       | Alt  |Ctrl/Esc|
- *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      | Home |       | PgUp |        |      |
- *                                 | Tab  | Space|------|       |------| Enter  | Bspc |
- *                                 |      |      | End  |       | PgDn |        |      |
- *                                 `--------------------'       `----------------------'
+ *                                        | PSCR | NOOP |       | NOOP | Pl/ps|
+ *                                 ,------|------|------|       |------+------+------.
+ *                                 |      |      | NOOP |       | NOOP |      |      |
+ *                                 | Tab  | Space|------|       |------| Enter| Bspc |
+ *                                 |      |      | NOOP |       | NOOP |      |      |
+ *                                 `--------------------'       `--------------------'
  */
 [BASE] = LAYOUT_ergodox_pretty(
   // left hand
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    XXXXXXX,              XXXXXXX,      KC_6,       KC_7,    KC_8,    KC_9,    KC_0,     LT(MDIA,KC_BSPC),
-  KC_DEL,  KC_Q,    KC_D,    KC_R,    KC_W,    KC_B,    LSA(KC_NO),           TG(UTIL),     KC_J,       KC_F,    KC_U,    KC_P,    KC_SCLN,  KC_BSLS,
+  KC_DEL,  KC_Q,    KC_D,    KC_R,    KC_W,    KC_B,    LSA(KC_NO),           LCA(KC_NO),   KC_J,       KC_F,    KC_U,    KC_P,    KC_SCLN,  KC_BSLS,
   KC_LCTL, KC_A,    KC_S,    KC_H,    KC_T,    KC_G,                                        KC_Y,       KC_N,    KC_E,    KC_O,    KC_I,     KC_QUOT,
-  KC_LSFT, KC_Z,    KC_X,    KC_M,    KC_C,    KC_V,    ALL_T(KC_NO),         MEH_T(KC_NO), KC_K,       KC_L,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-  ESCUT,   XXXXXXX, XXXXXXX, XXXXXXX, KC_LGUI,                                              MO(UTIL),   KC_LEFT, KC_UP,   KC_DOWN, KC_RIGHT,
-                                                           ALT_T(KC_APP), KC_LGUI,                KC_LALT, CTL_T(KC_ESC),
-                                                                          KC_HOME,                 KC_PGUP,
-                                                         KC_SPC, KC_TAB,  KC_END,                  KC_PGDN, KC_BSPC, KC_ENT
+  KC_LSFT, KC_Z,    KC_X,    KC_M,    KC_C,    KC_V,    HYPR(KC_NO),          MEH(KC_NO),   KC_K,       KC_L,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
+  ESCUT,   KC_LALT, XXXXXXX, XXXXXXX, KC_LGUI,                                              MO(UTIL),   KC_LEFT, KC_UP,   KC_DOWN, KC_RIGHT,
+                                              KC_PSCR,  XXXXXXX,              XXXXXXX,      KC_MPLY,
+                                                        XXXXXXX,              XXXXXXX,
+                                      KC_TAB, KC_SPC,   XXXXXXX,              XXXXXXX,      KC_ENT,     KC_BSPC
 ),
 /* Keymap 1: Utility layer
  *
