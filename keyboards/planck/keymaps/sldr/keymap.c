295d32d92524051e47ab0480a4323a6220d69bb7
Jonas A. Hultén <jonas@sldr.se>
2023-03-20 00:39:42 +0100

    Implemented tilde and grave key

--- keyboards/planck/keymaps/sldr/keymap.c
+++ keyboards/planck/keymaps/sldr/keymap.c
@@ -78,6 +78,7 @@ static uint16_t swe_held_kc;
 #define KC_SWE_QST_16 S(KC_MINS)
 #define KC_SWE_QOT_16 KC_BSLS
 #define KC_SWE_DQT_16 S(KC_2)
+#define KC_SWE_TIL_16 ALGR(KC_RBRC)
 // Gnarly as sin function macro to handle press/depress remapping
 #define remap(K, P) (P ? register_code(K) : unregister_code(K))
 #define remap16(K, P) (P ? register_code16(K) : unregister_code16(K))
@@ -147,7 +148,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_planck_grid(
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, PIPE,
+    SWE_TIL, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, PIPE,
     KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_LBRC, KC_RBRC, KC_EQL,  KC_PLUS, KC_CAPS,
     _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_LCBR, KC_RCBR, KC_MINS, KC_UNDS, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END
@@ -165,7 +166,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_planck_grid(
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,
+    SWE_GRV, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,
     KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_LBRC, KC_RBRC, KC_EQL,  KC_PLUS, KC_CAPS,
     _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_LCBR, KC_RCBR, KC_MINS, KC_UNDS, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END
@@ -288,6 +289,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       swescape(KC_QUOT, p);
       sweheld(SWE_QOT, p);
       remap_shift(KC_SWE_DQT_16, KC_SWE_QOT_16, p);
+      return false;
+    case SWE_TIL:                         // Tilde key
+      swescape(KC_GRV, p);
+      tap_code16(KC_SWE_TIL_16);
+      return false;
+    case SWE_GRV:                         // Grave key
+      swescape(KC_GRV, p);
+      tap_code(KC_SWE_ACT);
       return false;
   }
   return true;
