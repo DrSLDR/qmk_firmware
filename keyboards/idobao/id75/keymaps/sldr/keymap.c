4c6a7ce940c13bd47b90bd89abb5706459cac308
Jonas A. Hultén <jonas@sldr.se>
2024-10-20 14:54:01 +0200

    feat(id75): rebound some modifiers

--- keyboards/idobao/id75/keymaps/sldr/keymap.c
+++ keyboards/idobao/id75/keymaps/sldr/keymap.c
@@ -81,7 +81,8 @@ combo_t key_combos[COMBO_COUNT] = {
 };
 
 // Helper macros
-#define ALGUI A(KC_LGUI)
+#define ALSFT A(KC_LSFT)
+#define ALCTL A(KC_LCTL)
 #define KCLCA C(KC_LALT)
 #define ESCFN LT(_FN, KC_ESC)
 #define KCOSL OSL(_OSM)
@@ -96,20 +97,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
  * | TAB    | Q      | W      | E      | R      | T      | PREV   | PLAY   | NEXT   | Y      | U      | I      | O      | P      | \      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
- * | LCTRL  | A      | S      | D      | F      | G      | VOL-   | MUTE   | VOL+   | H      | J      | K      | L      | ;      | '      |
+ * | LCTRL  | A      | S      | D      | F      | G      | ALCTL  | MUTE   | VOL+   | H      | J      | K      | L      | ;      | '      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
- * | LSHIFT | Z      | X      | C      | V      | B      | ALGUI  | NOOP   | LCA    | N      | M      | ,      | .      | /      | RSHFT  |
+ * | LSHIFT | Z      | X      | C      | V      | B      | ALSFT  | NOOP   | LCA    | N      | M      | ,      | .      | /      | RSHFT  |
  * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+--------+-----------------+--------+--------|
- * | ESC/FN | LGUI   | OSL    | LALT   | FN     | SPACE  | CLSFT  | CAPS   | RALT   | ENTER  | FN     | LEFT   | UP     | DOWN   | RIGHT  |
+ * | ESC/FN | LGUI   | OSL    | LALT   | FN     | SPACE  | LALT   | CAPS   | RALT   | ENTER  | FN     | LEFT   | UP     | DOWN   | RIGHT  |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
 
   [_QW] = LAYOUT_ortho_5x15( /* QWERTY */
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_INS,  KC_PSCR, KC_PAUS, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MPRV, KC_MPLY, KC_MNXT, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
-    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_VOLD, KC_MUTE, KC_VOLU, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    ALGUI,   XXXXXXX, KCLCA,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-    ESCFN,   KC_LGUI, KCOSL,   KC_LALT, MO(_FN), KC_SPC,  CLSFT,   KC_CAPS, KC_RALT, KC_ENT,  MO(_FN), KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT  \
+    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    ALCTL,   KC_MUTE, KC_VOLU, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    ALSFT,   XXXXXXX, KCLCA,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+    ESCFN,   KC_LGUI, KCOSL,   KC_LALT, MO(_FN), KC_SPC,  KC_LALT, KC_CAPS, KC_RALT, KC_ENT,  MO(_FN), KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT  \
  ),
 
 /* WORKMAN (Partially transparent layer to handle Workman remappings; () means unchanged key, [] means to be changed key)
