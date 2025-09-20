e38787436db3f48a6004f05ea38b0864c643554b
Jonas A. Hultén <jonas@sldr.se>
2025-09-20 20:08:29 +0200

    fix(sldr): new map for é

--- keyboards/idobao/id75/keymaps/sldr/keymap.c
+++ keyboards/idobao/id75/keymaps/sldr/keymap.c
@@ -65,21 +65,21 @@ enum combos {
   AO_ARING,       // Å, å
   AE_ADIAE,       // Ä, ä
   EO_ODIAE,       // Ö, ö
-  EZ_EACUT,       // É, é
+  EJ_EACUT,       // É, é
   SS_CAPS         // Caps via shifts
 };
 
 const uint16_t PROGMEM ao_combo[]  = {KC_A, KC_O, COMBO_END};       // Å, å
 const uint16_t PROGMEM ae_combo[]  = {KC_A, KC_E, COMBO_END};       // Ä, ä
 const uint16_t PROGMEM eo_combo[]  = {KC_E, KC_O, COMBO_END};       // Ö, ö
-const uint16_t PROGMEM ez_combo[]  = {KC_E, KC_Z, COMBO_END};       // É, é
+const uint16_t PROGMEM ej_combo[]  = {KC_E, KC_J, COMBO_END};       // É, é
 const uint16_t PROGMEM ss_combo[]  = {KC_LSFT, KC_RSFT, COMBO_END}; // Caps
 
 combo_t key_combos[COMBO_COUNT] = {
   [AO_ARING] = COMBO_ACTION(ao_combo),      // Å, å
   [AE_ADIAE] = COMBO_ACTION(ae_combo),      // Ä, ä
   [EO_ODIAE] = COMBO_ACTION(eo_combo),      // Ö, ö
-  [EZ_EACUT] = COMBO_ACTION(ez_combo),      // É, é
+  [EJ_EACUT] = COMBO_ACTION(ej_combo),      // É, é
   [SS_CAPS] = COMBO_ACTION(ss_combo)        // Caps
 };
 
@@ -281,7 +281,7 @@ void process_combo_event(uint16_t combo_index, bool pressed) {
         tap_code16(RALT(KC_O));         // Ö, ö
       }
       break;
-    case EZ_EACUT:
+    case EJ_EACUT:
       if (pressed) {
         tap_code16(RALT(KC_G));         // É, é
       }
